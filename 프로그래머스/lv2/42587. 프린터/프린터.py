ac0c8f554d7f3fce633bdade11a604fc32f77774
Halim <78589670+4-trees-in-summer@users.noreply.github.com>
2023-01-24 20:31:12 +0900

    [level 2] Title: 프린터, Time: 0.18 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,28 +1,22 @@
-def solution(p, location):
-    cnt = 0
-    p1 = sorted(p, reverse=True)
-    
-    print(p1)
-    
-    while True :
-        if p[0] == p1[0]:
-            cnt += 1
-            
-            if location == 0 :
-                break
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    q = deque()
+    for idx, i in enumerate(priorities) :
+        q.append([i, idx])
+        
+    cnt = 1
+    while q :
+        if len(q) == 1 :
+            return cnt
+        
+        if q[0][0] >= max([num[0] for num in list(q)[1:]]) :
+            if q[0][1] == location :
+                return cnt
             else :
-                location -= 1
-                
-            p.pop(0)
-            p1.pop(0)
-            
+                q.popleft()
+                cnt += 1
         else :
-            temp = p.pop(0)
-            p.append(temp)
+            q.rotate(-1)
             
-            if location == 0 :
-                location += len(p)-1
-            else :
-                location -= 1
-    
-    return cnt

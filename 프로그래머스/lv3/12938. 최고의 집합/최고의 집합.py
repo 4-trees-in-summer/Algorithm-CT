e1ba89005125235f7bd9eb1eec62d3592df5be42
Halim <78589670+4-trees-in-summer@users.noreply.github.com>
2023-01-26 01:43:23 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
@@ -2,14 +2,17 @@ def solution(n, s):
     if n > s :
         return [-1]
     
-    answer = []
+    answer = [0 for _ in range(n)]
     
     for i in range(n) :
-        answer.append(s//n)
+        answer[i] = s//n
         
-    k = s%n
-    
-    for i in range(k) :
-        answer[-1-i] += 1
-        
-    return answer
+    if sum(answer) == s :
+        return answer
+    elif sum(answer) < s :
+        idx = -1
+        for i in range(s-sum(answer)) :
+            answer[idx] += 1
+            idx -= 1
+            
+        return answer

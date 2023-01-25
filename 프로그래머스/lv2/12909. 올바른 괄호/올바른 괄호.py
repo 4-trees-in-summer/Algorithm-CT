12b42c41d5ec3fc65304d5c25172c7deb04e5dc5
Halim <78589670+4-trees-in-summer@users.noreply.github.com>
2023-01-25 12:52:53 +0900

    [level 2] Title: 올바른 괄호, Time: 8.49 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,18 +1,16 @@
 def solution(s):
     answer = True
-    
     stack = []
+    
     for i in s :
         if i == '(' :
             stack.append(i)
-        elif i == ')' :
-            if not stack or stack[-1] == ')' :
+        else :
+            if not stack :
                 return False
             elif stack[-1] == '(' :
                 stack.pop()
-            
-    
     if stack :
-        return False 
+        return False
     else :
         return True

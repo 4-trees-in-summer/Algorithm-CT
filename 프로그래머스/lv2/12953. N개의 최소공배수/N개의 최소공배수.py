fcf0e60e3029c1fb642c73b9762884ee03204d27
Halim <78589670+4-trees-in-summer@users.noreply.github.com>
2023-01-25 16:26:32 +0900

    [level 2] Title: N개의 최소공배수, Time: 193.73 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,9 +1,15 @@
 import math
 
 def solution(arr):
+    ans = []
     answer = arr[0]
     
     for i in arr[1:] :
-        answer = answer*i//math.gcd(answer,i)
+        t = max(answer, i)
+        
+        for j in range(t, answer*i+1) :
+            if j%answer == 0 and j%i == 0 :
+                answer = j
+                break
         
     return answer

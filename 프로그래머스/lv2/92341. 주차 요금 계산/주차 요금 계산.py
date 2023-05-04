0f0d3ba5f88163f30513cab55d392abe08058997
Halim <78589670+4-trees-in-summer@users.noreply.github.com>
2023-05-04 09:28:50 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+def solution(fees, records):
+    
+    car = defaultdict(list)
+    
+    for record in records :
+        record = record.split()
+        
+        # if record[1] not in car.keys() :
+             
+        car[record[1]].append(record[0].replace(':', ''))
+        
+    keys = sorted(list(car.keys()))
+    print(keys)
+    
+    answer = [0 for i in range(len(keys))]
+    for ans, num in enumerate(keys) :
+        if len(car[num])%2 == 1 :
+            car[num].append('2359')
+            
+        time_h, time_m = 0, 0
+        for idx, t in enumerate(car[num]) :
+            if idx%2 == 0 :
+                continue
+            time_h += int(car[num][idx][:2]) - int(car[num][idx-1][:2])
+            time_m += int(car[num][idx][2:]) - int(car[num][idx-1][2:])
+        
+        
+        time = time_h*60 + time_m
+            
+        answer[ans] += fees[1]
+        if time > fees[0] :
+            if (time-fees[0])%fees[2] == 0:
+                answer[ans] += fees[3]*((time-fees[0])//fees[2])
+            else :
+                answer[ans] += fees[3]*(((time-fees[0])//fees[2])+1)
+
+    return answer

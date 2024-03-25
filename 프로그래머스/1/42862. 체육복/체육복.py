9fe1bb1c940dc9ad3b76bdc720046769dd6a5779
Halim Lim <78589670+halimx2@users.noreply.github.com>
2024-03-25 21:04:21 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,17 +1,17 @@
 def solution(n, lost, reserve):
     answer = 0
-    lost.sort()
-    reserve.sort()
     
-    reserved = 0
-    lost_ = list(set(lost)-set(reserve))
-    reserve_ = list(set(reserve)-set(lost))
-
+    lost_ = list(set(lost) - set(reserve))
+    reserve_ = list(set(reserve) - set(lost))
+    already_reserved = []
+    
     for l in lost_ :
         for r in reserve_ :
+            if r in already_reserved :
+                continue
+            
             if abs(r-l) <= 1 :
-                reserved += 1
-                reserve_.remove(r)
+                already_reserved.append(r)
                 break
     
-    return n - len(lost_) + reserved
+    return n - len(lost_) + len(already_reserved)

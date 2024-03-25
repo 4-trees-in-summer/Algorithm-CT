022b04ac02cf824ef34eb8be3ed6005e30eb8622
Halim Lim <78589670+halimx2@users.noreply.github.com>
2024-03-25 23:05:35 +0900

    [level 2] Title: 구명보트, Time: 10.26 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,18 +2,21 @@ from collections import deque
 
 def solution(people, limit):
     answer = 0
-    people.sort(reverse=True)
-    
+            
+    people.sort()
     q = deque(people)
-    while q:
-        if len(q) <= 1 :
+    
+    while q :
+        if len(q) == 1 :
             answer += 1
             break
-        if q[0] + q[-1] > limit :
+            
+        maxi = q.pop()
+        mini = q[0]
+        
+        if mini+maxi <= limit :
             q.popleft()
-        else :
-            q.popleft()
-            q.pop()
-        answer += 1
-    
+        
+        answer += 1 
+        
     return answer

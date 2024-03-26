93be63c6cee869f0503d2b50acbd9a10782a0790
Halim Lim <78589670+halimx2@users.noreply.github.com>
2024-03-27 00:05:15 +0900

    [level 3] Title: 네트워크, Time: 4.93 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,25 +1,27 @@
 from collections import deque
 
-def bfs(num, computers, visited, answer) :
-    q = deque()
-    q.append(num)
-    
-    while q :
-        computer = q.popleft()
-        visited[computer] = True
-        
-        for idx, link in enumerate(computers[computer]) :
-            if not visited[idx] and link == 1 :
-                q.append(idx)
-    
-    return answer+1, visited
-
 def solution(n, computers):
     answer = 0
-    visited = [False] * n
+    visited = []
+    
+    for n_ in range(n) :
+        if n_ in visited :
+            continue
+            
+        q = deque([n_])
 
-    for num in range(n) :
-        if not visited[num] :
-            answer, visited = bfs(num, computers, visited, answer)
+        while q : 
+            idx = q.popleft()
+            visited.append(idx)
+            
+            for idx_, computer in enumerate(computers[idx]) :
+                if idx_ not in visited and computer == 1:
+                    q.append(idx_)
         
+        if visited == list(range(n)) :
+            answer += 1
+            break
+        else :
+            answer += 1
+                
     return answer
